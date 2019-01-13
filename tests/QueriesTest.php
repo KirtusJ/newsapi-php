792fa51d7ec5fcb90129f00b8e1937bbb8725847
KirtusJ <kirtuswork@gmail.com>
2019-01-12 18:14:03 -0800

    Added some more tests

--- tests/QueriesTest.php
+++ tests/QueriesTest.php
@@ -2,9 +2,39 @@
 use PHPUnit\Framework\TestCase;
 
 class QueriesTest extends TestCase {
-  public function testIsThereAnySyntaxError() {
-	   $var = new kirtusj\newsapi\Queries;
-	   $this->assertTrue(is_object($var));
-	   unset($var);
+  private $QueriesClass;
+
+  public function setUp() {
+    parent::setUp();
+    $this->QueriesClass = new kirtusj\newsapi\Queries;
+  }
+  public function tearDown() {
+    $this->QueriesClass = null;
+    parent::tearDown();
+  }
+  /** @test **/
+  public function get_query() {
+  	$query = array('q1' => 'trump', 'q2' => 1);
+  	$payload = [];
+  	$this->assertInternalType('array', $this->QueriesClass->get_query($query['q1'], $payload));
+  	$this->expectException(InvalidArgumentException::class);
+  	$this->assertInternalType('array', $this->QueriesClass->get_query($query['q2'], $payload));
+  }
+  /** @test **/
+  public function get_date() {
+  	$query = array('date1' => '2018-02-21', 'date2' => '21-02-2018', 'date3' => 2018);
+  	$payload = [];
+  	$this->assertInternalType('array', $this->QueriesClass->get_date($query['date1'], $payload, 'from'));
+  	$this->expectException(InvalidArgumentException::class);
+  	$this->assertInternalType('array', $this->QueriesClass->get_date($query['date2'], $payload, 'from'));
+  	$this->assertInternalType('array', $this->QueriesClass->get_date($query['date3'], $payload, 'from'));
+  }
+  /** @test **/
+  public function get_category() {
+  	$query = array('cat1' => 'business', 'cat2' => 'obama');
+  	$payload = [];
+  	$this->assertInternalType('array', $this->QueriesClass->get_category($query['cat1'], $payload));
+  	$this->expectException(UnexpectedValueException::class);
+  	$this->assertInternalType('array', $this->QueriesClass->get_category($query['cat2'], $payload));
   }
 }
--- tests/ScraperTest.php
+++ tests/ScraperTest.php
@@ -12,22 +12,29 @@ class ScraperTest extends TestCase {
     $this->ScraperClass = null;
     parent::tearDown();
   }
+
+  /** @test **/
+  public function wrong_api_key() {
+    $class = new kirtusj\newsapi\Scraper($api_key="wrong_api_key");
+    $this->assertTrue(strpos($class->get_top(array('q' => 'truimp')), '"status":"error"') == true);
+  }
+
   /** @test **/
   public function get_top() {
-    $this->assertTrue(strpos($this->ScraperClass->get_top(array('q' => 'trump')), '"status":"ok"') !==  false);
+    $this->assertTrue(strpos($this->ScraperClass->get_top(array('q' => 'trump')), '"status":"ok"') == true);
     $this->expectException(InvalidArgumentException::class);
-    $this->assertTrue(strpos($this->ScraperClass->get_top(array('page' => '1')), '"status":"ok"') !==  false);
+    $this->assertTrue(strpos($this->ScraperClass->get_top(array('page' => '1')), '"status":"ok"') == true);
   }
   /** @test **/
   public function get_everything() {
-    $this->assertTrue(strpos($this->ScraperClass->get_everything(array('sources' => 'cnn')), '"status":"ok"') !== false);
+    $this->assertTrue(strpos($this->ScraperClass->get_everything(array('sources' => 'cnn')), '"status":"ok"') == true);
     $this->expectException(UnexpectedValueException::class);
-    $this->assertTrue(strpos($this->ScraperClass->get_everything(array('sortBy' => 'idk')), '"status":"ok"') !== false);
+    $this->assertTrue(strpos($this->ScraperClass->get_everything(array('sortBy' => 'idk')), '"status":"ok"') == true);
   }
   /** @test **/
   public function get_sources() {
-    $this->assertTrue(strpos($this->ScraperClass->get_sources(array('category' => 'business')), '"status":"ok"') !== false);
+    $this->assertTrue(strpos($this->ScraperClass->get_sources(array('category' => 'business')), '"status":"ok"') == true);
     $this->expectException(UnexpectedValueException::class);
-    $this->assertTrue(strpos($this->ScraperClass->get_sources(array('category' => 'trump')), '"status":"ok"') !== false);
+    $this->assertTrue(strpos($this->ScraperClass->get_sources(array('category' => 'trump')), '"status":"ok"') == true);
   }
 }
